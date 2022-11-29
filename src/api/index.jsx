cfc41823ae338432234b629533f08e3915981c12
usmanmughal-mirza <ubzafar2001@gmail.com>
2022-11-28 22:27:15 -0800

    nodejs api change

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API=axios.create({baseURL:'http://127.0.0.1:8000'}) 
-const API=axios.create({baseURL:'https://memories-project-baig2.herokuapp.com/'})
+const API=axios.create({baseURL:'https://js-mastery-memories.herokuapp.com/'})
 
 // set headers 
 
